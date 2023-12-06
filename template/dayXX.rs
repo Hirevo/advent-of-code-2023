287839657629050a09be41fa2ea4779057bb82da
Nicolas Polomack <nicolas@polomack.eu>
2023-12-06 18:11:27 +0100

    refactor: slightly improved code templates

--- template/dayXX.rs
+++ template/dayXX.rs
@@ -8,15 +8,21 @@ pub const INPUT_PATH: &str = "inputs/dayXX.txt";
 type InputType = String;
 
 fn part1(_input: &InputType) -> Result<(), Error> {
-    todo!()
+    let answer: u64 = todo!();
+    println!("part1: {answer}");
+    Ok(())
 }
 
 fn part2(_input: &InputType) -> Result<(), Error> {
-    todo!()
+    let answer: u64 = todo!();
+    println!("part2: {answer}");
+    Ok(())
 }
 
 pub fn run() -> Result<(), Error> {
     let input = fs::read_to_string(INPUT_PATH)?;
+
+    let input: InputType = todo!();
 
     measured!(part1(&input))?;
     measured!(part2(&input))?;
